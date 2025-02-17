2c6968e6db06ed195ceb0c151c1f6ad040709f39
Philipp Fath <41743364+felipeDelMundo@users.noreply.github.com>
2025-02-17 13:09:28 +0100

    Initial commit

---
+++ src/main/java/org/optsol/jdecor_pojo_template/model/constants/Constants.java
@@ -0,0 +1,31 @@
+package org.optsol.jdecor_pojo_template.model.constants;
+
+import java.util.List;
+import java.util.Map;
+import lombok.Value;
+import org.optsol.jdecor.core.IConstants;
+
+@Value
+public class Constants implements IConstants {
+  //SETS
+  List<Integer> _C;
+  List<Integer> _M;
+  //PARAMETERS
+  Map<Integer, Double> _Q;
+  Map<Integer, Double> _P;
+  Map<Integer, Map<Integer, Double>> _N;
+
+  public double get_Q(int m) {
+    return get_Q().get(m);
+  }
+
+  public double get_P(int c) {
+    return get_P().get(c);
+  }
+
+  public double get_N(
+      int c,
+      int m) {
+    return get_N().get(c).getOrDefault(m, 0.);
+  }
+}
